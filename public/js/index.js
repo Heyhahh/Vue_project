ffd744d5ea95f325e44c58f813d09459ce0563d6
Heyhahh <Honeylombs@163.com>
2019-06-26 01:43:25 +0800

    修改

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,36 @@
    new Vue({
-        el: "#f1",
-        data: {
-          p1: result[0],
-          p2: result[1],
-          p3: result[2],
-          pOthers: result.slice(3)
-        }
-      })
+       el: "#f1",
+       data: {
+           p1: { price: 0 },
+           p2: { price: 0 },
+           p3: { price: 0 },
+           pOthers: []
+       },
+       created() {
+           axios.get("http://localhost:3000/index")
+               .then(result => {
+                   console.log(result.data);
+                   this.p1 = result.data[0];
+                   this.p2 = result.data[1];
+                   this.p3 = result.data[2];
+                   this.pOthers = result.data.slice(3);
+               })
+       },
+       watch: {
+           p1() {
+               console.log(this.p1)
+           },
+           p2() {
+               console.log(this.p2)
+           },
+           p3() {
+               console.log(this.p3)
+           },
+           p4() {
+               console.log(this.p4)
+           },
+           pOthers() {
+               console.log(this.pOthers)
+           }
+       },
+   })
