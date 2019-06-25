027c58cb933451cb6805aca1991f9fb79f21f4b2
Heyhahh <Honeylombs@163.com>
2019-06-25 18:06:54 +0800

    Vue的项目

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+   new Vue({
+        el: "#f1",
+        data: {
+          p1: result[0],
+          p2: result[1],
+          p3: result[2],
+          pOthers: result.slice(3)
+        }
+      })
